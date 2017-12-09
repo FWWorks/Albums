8975cd8069eb61b2d2a620d9cf4b53cdf17b7ee9
fw14 <214731923@qq.com>
2017-12-10 01:55:22 +0800

    LF will be replaced by CRLF

--- index.php
+++ index.php
@@ -1,4 +1,195 @@
 <?php
-
+date_default_timezone_set("Asia/Shanghai");
+session_start();
+@$my_id = $_SESSION["user_id"];
 ?>
 
+<html lang="zh-CN">
+<head>
+<meta charset="UTF-8">
+<title>iAlbum</title>
+<meta name="keywords" content="iAlbum" />
+<meta name="description" content="" />
+<link rel="stylesheet" href="css/index.css"/>
+<link rel="stylesheet" href="css/style.css"/>
+<script type="text/javascript" src="js/jquery1.42.min.js"></script>
+<script type="text/javascript" src="js/jquery.SuperSlide.2.1.1.js"></script>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <!--header start-->
+    <div id="header">
+      <h1>iAlbum</h1>
+      <h1><?= $user_id ?>aaa</h1>
+      <p>Confidence and Smile</p>    
+    </div>
+     <!--header end-->
+    <!--nav-->
+     <div id="nav">
+        <ul>
+         <li><a href="index.html">Moments</a></li>
+         <li><a href="albums.php">Photos</a></li>
+         <li><a href="shuo.html">Activities</a></li>
+         <li><a href="riji.html">Me</a></li>
+         
+         <div class="clear"></div>
+        </ul>
+      </div>
+       <!--nav end-->
+    <!--content start-->
+    <div id="content">
+         <!--left-->
+         <div class="left" id="c_left">
+           <div class="s_tuijian">
+
+           <h2>Recommendation</h2>
+           </div>
+          <div class="content_text">
+           <!--wz-->
+           <div class="wz">
+            <h3><a href="#" title="标题1">标题1</a></h3>
+             <dl>
+               <dt><img src="images/s.jpg" width="200"  height="123" alt=""></dt>
+               <dd>
+                 <p class="dd_text_1">描述1</p>
+               <p class="dd_text_2">
+               <span class="left author">用户名</span><span class="left sj">时间</span>
+               <span class="left yd"><a href="#" title="view">view</a>
+               </span>
+                <div class="clear"></div>
+               </p>
+               </dd>
+               <div class="clear"></div>
+             </dl>
+            </div>
+           <!--wz end-->
+            <!--wz-->
+           <div class="wz">
+            <h3><a href="#" title="标题1">标题1</a></h3>
+             <dl>
+               <dt><img src="images/s.jpg" width="200"  height="123" alt=""></dt>
+               <dd>
+                 <p class="dd_text_1">描述1</p>
+               <p class="dd_text_2">
+               <span class="left author">用户名</span><span class="left sj">时间</span>
+               <span class="left yd"><a href="#" title="view">view</a>
+               </span>
+                <div class="clear"></div>
+               </p>
+               </dd>
+               <div class="clear"></div>
+             </dl>
+            </div>
+           <!--wz end-->
+            <!--wz-->
+           <div class="wz">
+            <h3><a href="#" title="标题1">标题1</a></h3>
+             <dl>
+               <dt><img src="images/s.jpg" width="200"  height="123" alt=""></dt>
+               <dd>
+                 <p class="dd_text_1">描述1</p>
+               <p class="dd_text_2">
+               <span class="left author">用户名</span><span class="left sj">时间</span>
+               <span class="left yd"><a href="#" title="view">view</a>
+               </span>
+                <div class="clear"></div>
+               </p>
+               </dd>
+               <div class="clear"></div>
+             </dl>
+            </div>
+           <!--wz end-->
+           <!--wz-->
+           <div class="wz">
+            <h3><a href="#" title="标题1">标题1</a></h3>
+             <dl>
+               <dt><img src="images/s.jpg" width="200"  height="123" alt=""></dt>
+               <dd>
+                 <p class="dd_text_1">描述1</p>
+               <p class="dd_text_2">
+               <span class="left author">用户名</span><span class="left sj">时间</span>
+               <span class="left yd"><a href="#" title="view">view</a>
+               </span>
+                <div class="clear"></div>
+               </p>
+               </dd>
+               <div class="clear"></div>
+             </dl>
+            </div>
+           <!--wz end-->
+            <!--wz-->
+           <div class="wz">
+            <h3><a href="#" title="标题1">标题1</a></h3>
+             <dl>
+               <dt><img src="images/s.jpg" width="200"  height="123" alt=""></dt>
+               <dd>
+                 <p class="dd_text_1">描述1</p>
+               <p class="dd_text_2">
+               <span class="left author">用户名</span><span class="left sj">时间</span>
+               <span class="left yd"><a href="#" title="view">view</a>
+               </span>
+                <div class="clear"></div>
+               </p>
+               </dd>
+               <div class="clear"></div>
+             </dl>
+            </div>
+           <!--wz end-->
+              
+                             
+           </div>
+         </div>
+         <!--left end-->
+         <!--right-->
+         <div class="right" id="c_right">
+          <div class="s_about">
+           <img src="images/me.jpg" width="230" height="230" alt=""/>
+           <p>用户名：xx</p>
+           <p>简介：</p>
+           <p>
+           <br></br>
+           <div class="clear"></div>
+           </p>
+          </div>
+          <!--栏目分类-->
+           <div class="lanmubox">
+              <div class="hd">
+               <ul><li>关注者动态</li><li>粉丝动态</li><li class="hd_3">我的动态</li></ul>
+              </div>
+              <div class="bd">
+                <ul>
+					
+					
+					
+				</ul>
+                 <ul>
+					
+					
+				</ul>
+                 <ul>
+					
+					
+				</ul>
+                 
+                
+              </div>
+           </div>
+           <!--end-->
+           
+         </div>
+         <!--right end-->
+         <div class="clear"></div>
+    </div>
+    <!--content end-->
+    <!--footer start-->
+    <div id="footer">
+     
+    </div>
+    <!--footer end-->
+    <script type="text/javascript">jQuery(".lanmubox").slide({easing:"easeOutBounce",delayTime:400});</script>
+    <script  type="text/javascript" src="js/nav.js"></script>
+</body>
+</html>
--- login.php
+++ login.php
@@ -23,31 +23,74 @@ else
 
 ?>
 
-<html>
+ <html lang="en" class="no-js"> <!--<![endif]-->
     <head>
-		<meta charset="UTF-8">
-		<title>登录</title>
-		<script src="jquery.js"></script>
-	</head>
+        <meta charset="UTF-8" />
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
+        <title>Login</title>
+        <script src="jquery.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+        <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style1.css" />
+		<link rel="stylesheet" type="text/css" href="css/animate-custom.css" />
+    </head>
     <body>
-		<form action="login.php" method="post" id="form">
-			<table>
-				<tr>
-					<td>用户名</td>
-					<td><input type="text" name="name" id="name"></td>
-				</tr>
-				<tr>
-					<td>密码</td>
-					<td><input type="password" name="password" id="password"></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type="button" value="登录" onclick="on_submit()"></td>
-				</tr>
-			</table>
-		</form>
+        <div class="container">
+            <!-- Codrops top bar -->
+            <div class="codrops-top">
+                
+                <span class="right">
+                   
+                </span>
+                <div class="clr"></div>
+            </div><!--/ Codrops top bar -->
+            <header>
+                <h1>Welcome to <span>iAlbum</span></h1>
+				<nav class="codrops-demos">
+					
+				</nav>
+            </header>
+            <section>				
+                <div id="container_demo" >
+                    
+                    <a class="hiddenanchor" id="toregister"></a>
+                    <a class="hiddenanchor" id="tologin"></a>
+                    <div id="wrapper">
+                        <div id="login" class="animate form">
+                            <form  action="login.php" autocomplete="on" method="post" id="form"> 
+                                <h1>Log in</h1> 
+                                <p> 
+                                    <label for="username" class="uname" data-icon="u" > Your email or username </label>
+                                    <input type="text" name="name" id="name"/>
+                                </p>
+                                <p> 
+                                    <label for="password" class="youpasswd" data-icon="p"> Your password </label>
+                                    <input type="password" name="password" id="password" /> 
+                                </p>
+                                <p class="keeplogin"> 
+									<input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping" /> 
+									<label for="loginkeeping">Keep me logged in</label>
+								</p>
+                                <p class="login button"> 
+                                    <input type="submit" value="Login" onclick="on_submit()"/> 
+								</p>
+                                <p class="change_link">
+									Not a member yet ?
+									<a href="#toregister" class="to_register">Join us</a>
+								</p>
+                            </form>
+                        </div>
+						
+                    </div>
+                </div>  
+            </section>
+        </div>
     </body>
-	<script>
+    <script>
         
 <?php
 
@@ -75,3 +118,57 @@ else
 		}
 	</script>
 </html>
+
+<!-- <html>
+    <head>
+		<meta charset="UTF-8">
+		<title>登录</title>
+		<script src="jquery.js"></script>
+	</head>
+    <body background="img1.JPG">
+		<form action="login.php" method="post" id="form">
+			<table>
+				<tr>
+					<td>用户名 </td>
+					<td><input type="text" name="name" id="name"></td>
+				</tr>
+				<tr>
+					<td>密码</td>
+					<td><input type="password" name="password" id="password"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="button" value="登录" onclick="on_submit()"></td>
+				</tr>
+			</table>
+		</form>
+    </body>
+	<script>
+        
+<?php
+
+//	if($error != null)
+//		echo "alert(\"$error\")";
+
+?>
+
+		function on_submit()
+		{
+			var name = $("#name").val();
+			var password = $("#password").val();
+			if(name == "")
+			{
+				alert("用户名不能为空！");
+				return false;
+			}
+			if(password == "")
+            {
+				alert("密码不能为空！");
+				return false;
+			}
+			$("#form").submit();
+			return true;
+		}
+	</script>
+</html>
+ -->
